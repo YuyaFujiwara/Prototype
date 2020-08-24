e5f41aba530efc0f1ec298de6a3915cc2da0a762
s1250109 <ariadastmotonobu@gmail.com>
2020-08-24 17:59:08 +0900

    new one

--- Prototype.py
+++ Prototype.py
@@ -1,5 +1,7 @@
 import nltk
 import pprint
+import re
+from operator import itemgetter
 
 from nltk import word_tokenize
 
@@ -7,6 +9,7 @@ Sentence="Two frogs, a father and his son, accidently fell into a bucket of milk
 
 T_sentence=nltk.word_tokenize(Sentence)
 Tagged=nltk.pos_tag(T_sentence)
+instancelist=[]
 
 print(Tagged)
 print('\n')
@@ -15,33 +18,43 @@ print("1:")#progressive aspect
 for tag in Tagged: 
 	if tag[1]=="VBG":
 		print(tag)
-		
+		instancelist.append(tag)
 print('\n')
 		
 print("2:")#perfect aspect
 for tag in Tagged: 
 	if tag[0]=="had" or tag[0]=="have":
 		print(tag)
-
+		instancelist.append(tag)
 print('\n')
 
 print("3:")#past tense
 for tag in Tagged:
 	if tag[1]=="VBD":
 		print(tag)
-	
+		instancelist.append(tag)
 print('\n')
 
-print("4:")#regular past tense
+print("4:")#regular past tense 
 for tag in Tagged:
-	if tag[1]=="VBD":
+	if tag[1]=="VBD" and tag[0]==r'.*ed$': #<-I do not know why this line does not work.
 		print(tag)
-	
+		instancelist.append(tag)
 print('\n')
 
 print('5')#underline each verb group
-for i,tag in Tagged:
-	if tag[1]=="VB" or tag[1]=="VBD" or tag[1]=="VBG" or tag[1]=="VBN" or tag[1]=="VBP" or tag[1]=="VBZ":
+for i,tag in enumerate(Tagged):
+	if tag[1]=="VB"or tag[1]=="VBD"or tag[1]=="VBG" or tag[1]=="VBN" or tag[1]=="VBP" or tag[1]=="VBZ":
+		'''print(tag)
+		print(Tagged[i+1])
+		if tag[1]=="IN":''' #I tried to make "IN(preposition)+VB~"->verb group but it does not work.
 		print(tag)
-	#	print(Tagged[i])
+		print(Tagged[i+1])
 		
+print('\n')
+
+print('6') #found instances in alphabetical list
+thelist=set(instancelist)
+instancelist=list(thelist)
+sorted(instancelist,key=itemgetter(0))#sorted does not work well, disorder.
+print(instancelist)
